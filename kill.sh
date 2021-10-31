5868e757dd0e94b09593ecb71fdb3228595144a6
gangmin <wjdrmf314@gmail.com>
2021-10-31 14:23:51 +0900

    emr, env, iot

--- kill.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-PID=`ps -ef | grep “/home/ec2-user/server.py” | grep -v “grep | awk '{print $2}'`
-echo “kill $PID”
-kill -9 $PID
-
-PID=`ps -ef | grep “/home/ec2-user/client.py” | grep -v “grep | awk '{print $2}'`
-echo “kill $PID”
-kill -9 $PID
