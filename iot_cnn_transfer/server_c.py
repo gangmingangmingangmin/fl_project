a0c1a6df6937cdfff3e9648422abf016ac056702
gangmin <wjdrmf314@gmail.com>
2021-12-10 03:34:40 +0000

    transfer learning

---
+++ iot_cnn_transfer/server_c.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python3.7
+
+import flwr as fl
+import warnings
+warnings.simplefilter(action='ignore',category = FutureWarning)
+from typing import Callable, Dict, Optional, Tuple
+from sklearn.metrics import classification_report, confusion_matrix,log_loss
+import sys
+import tensorflow as tf
+from tensorflow.keras import layers
+import tensorflow.keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten,Conv2D, MaxPooling2D
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.linear_model import LogisticRegression
+np.random.seed(10)
+
+MIN_AVAILABLE_CLIENTS=int(sys.argv[1])
+NUM_ROUND=5
+NUM_EPOCHS = 1
+
+#data load from boto3
+def divide_list(arr,n):
+    for i in range(0,len(arr),n):
+        yield arr[i:i+n]
+
+
+
+
+# 데이터 수를 노드 수에 맞추기 위한 코드
+file_n = 5000 #고정 50000
+
+# 나머지 분배코드, 사용x
+if file_n % MIN_AVAILABLE_CLIENTS > 0:
+    DIV = file_n // MIN_AVAILABLE_CLIENTS +1
+else:
+    DIV = file_n//MIN_AVAILABLE_CLIENTS
+print(file_n,DIV)
+
+
+#fit strategy
+def get_on_fit_config_fn() -> Callable[[int], Dict[str, str]]:
+    def fit_config(rnd:int) -> Dict[str,str]:
+        config = {"epoch" : NUM_EPOCHS, "round":rnd, "file_n":file_n, "div":DIV, "n_round":NUM_ROUND,"n_clients": MIN_AVAILABLE_CLIENTS}
+        return config
+    return fit_config
+#server side evaluation
+def get_eval_fn(model):
+    #read file from s3
+    import boto3
+    client = boto3.client('s3')
+    bucket = 'federatedlearning2'
+    s3 = boto3.resource('s3',region_name = 'ap-northeast-2')
+    #python 2 버전에서 dump한 파일이기때문에 encoding, python 3 버전은 bytes 사용
+    obj = s3.Object(bucket,'mnist/X_test.pickle')
+    objd=obj.get()['Body'].read()
+    X_test = pickle.loads(objd,encoding='bytes')
+
+    obj = s3.Object(bucket,'mnist/y_test.pickle')
+    objd=obj.get()['Body'].read()
+    y_test = pickle.loads(objd,encoding='bytes')
+    
+    X_test = X_test.reshape(X_test.shape[0],img_row,img_col,1)
+    X_test = X_test.astype('float32')/255
+    y_test = tf.keras.utils.to_categorical(y_test,10)
+    # The `evaluate` function will be called after every round
+    def evaluate(weights: fl.common.Weights) -> Optional[Tuple[float, float]]:
+        
+        #print(X_test.shape)
+        model.set_weights(weights)  # Update model with the latest parameters
+        #x_test, y_test
+        predict = model.predict(X_test)
+        y_pred = np.argmax(predict,axis=1)
+        y_label = np.argmax(y_test,axis=1)
+        #classification_report, confusion_matrix
+        cr = classification_report(y_label,y_pred,digits = 4)
+        cm = confusion_matrix(y_label,y_pred)
+        f = open('/home/ec2-user/result.txt','w')
+        f.write("classification_report : "+str(cr)+"\n")
+        f.write("confusion_matrix : "+str(cm))
+        f.close()
+        #할당제거
+        return 0, {"err":0}
+
+    return evaluate
+
+#model for cnn
+img_row = 28
+img_col = 28
+input_shape = (img_row,img_col,1)
+
+
+batch_size = 128
+num_classes = 10
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1), padding='same',
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Conv2D(64, (2, 2), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(1000, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+#logistic model
+#model = LogisticRegression()
+#set initial parameters
+'''
+n_classes = 10  # MNIST has 10 classes
+n_features = 784  # Number of features in dataset
+model.classes_ = np.array([i for i in range(10)])
+
+model.coef_ = np.zeros((n_classes, n_features))
+model.intercept_ = np.zeros((n_classes,))
+'''
+strategy = fl.server.strategy.FedAvg(
+    fraction_fit=1,  # Sample 10% of available clients for the next round
+    min_fit_clients=MIN_AVAILABLE_CLIENTS,  # Minimum number of clients to be sampled for the next round
+    min_available_clients=MIN_AVAILABLE_CLIENTS,  # Minimum number of clients that need to be connected to the server before a training round can start
+    #min_eval_clients=MIN_AVAILABLE_CLIENTS, # default = 2
+    on_fit_config_fn=get_on_fit_config_fn(),
+    eval_fn = get_eval_fn(model)
+)
+print('clients:'+str(MIN_AVAILABLE_CLIENTS))
+import time
+#federated learning
+fl.server.start_server(config={"num_rounds": NUM_ROUND},strategy=strategy)
+
