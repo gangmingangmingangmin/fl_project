681c1225d66b8739063603f04bdcabea2b2453a6
gangmin <wjdrmf314@gmail.com>
2021-12-13 05:54:32 +0000

    lstm transfer learning

---
+++ iot_transfer/client_g_t.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python3.7
+
+import flwr as fl
+import math
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.utils import Sequence
+from datetime import timedelta
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+import pickle
+import numpy as np
+import pandas as pd
+import time
+import os
+import boto3
+#load data and fit
+##############################################
+import pickle
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.utils import Sequence
+import time
+import sys
+##############################################
+if len(sys.argv[1]) == 3:
+  gid = int(sys.argv[1][0])-1
+  cid = int(sys.argv[1][2:])-1
+else:
+  cid = int(sys.argv[1][3:])-1
+##############################################
+#loader
+class TimeSeriesLoader:
+    def __init__(self,file_n,div,n_clients):
+        if n_clients>9:
+          self.start_index=gid*div+cid*6
+        else:
+          self.start_index=cid*div
+        #min_n = min(file_n,self.start_index+div)# 파일 크기 안넘도록
+        #self.num_files = min_n-self.start_index
+        self.num_files = div
+        self.files_indices = np.arange(self.num_files)
+        self.shuffle_chunks()
+    def num_chunks(self):
+        return self.num_files
+
+    def shuffle_chunks(self):
+        np.random.shuffle(self.files_indices)
+
+    def get_chunk(self, idx):
+        # model.fit does train the model incrementally. ie. Can call m
+        assert (idx >= 0) and (idx < self.num_files)
+
+        #data load from s3
+        ind = self.files_indices[idx]+self.start_index # data index
+        s3 = boto3.resource('s3',region_name='ap-northeast-2')
+        bucket = 'federatedlearning2'
+
+        obj=s3.Object(bucket,'ts_file'+str(ind)+'.pkl')
+
+        objd = obj.get()['Body'].read()
+        df_ts = pickle.loads(objd)
+
+        num_records = len(df_ts.index)
+
+        features = df_ts.drop('y', axis=1).values
+        target = df_ts['y'].values
+
+        # reshape for input into LSTM. Batch major format.
+        features_batchmajor = np.array(features).reshape(num_records, -1, 1)
+        return features_batchmajor, target
+
+# Load and compile Keras model
+ts_inputs = tf.keras.Input(shape=(1008,1))
+x = layers.LSTM(units=10)(ts_inputs)
+x = layers.Dropout(0.2)(x)
+outputs = layers.Dense(1, activation='linear')(x)
+model = tf.keras.Model(inputs=ts_inputs, outputs=outputs)
+
+model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.01),
+          loss=tf.keras.losses.MeanSquaredError(),
+          metrics=['mse'])
+
+with open('/home/ec2-user/full_lstm.pkl','rb') as f:
+  w = pickle.load(f)
+model.set_weights(w)
+for layer in model.layers[:3]:
+  layer.trainable=False
+
+# Define Flower client
+class flClient(fl.client.NumPyClient):
+    def get_parameters(self):
+        return model.get_weights()[3:]
+
+    def fit(self,parameters,config):
+        new_weights = model.get_weights()[:3] + parameters
+        model.set_weights(new_weights)
+        tss=TimeSeriesLoader(config['file_n'],config['div'],config['n_clients'])
+        BATCH_SIZE = 128
+        NUM_EPOCHS = config['epoch']
+        NUM_CHUNKS = tss.num_chunks()
+        rnd = config['round']-1
+        NUM_CHUNKS_LIST=[]
+        index=0
+        r=NUM_CHUNKS//config['n_round']
+        for i in range(config['n_round']):
+            NUM_CHUNKS_LIST.append([index,index+r])
+            index = index+r
+        NUM_CHUNKS_LIST[-1][-1] = NUM_CHUNKS
+        x_len=0
+        for epoch in range(NUM_EPOCHS):
+            print('epoch #{}'.format(epoch))
+            #for i in range(NUM_CHUNKS_LIST[rnd][0],NUM_CHUNKS_LIST[rnd][1]):
+            for i in range(NUM_CHUNKS):
+                X, y = tss.get_chunk(i)
+                x_len+=len(X)
+                model.fit(x=X, y=y, batch_size=BATCH_SIZE)
+        
+        return model.get_weights()[3:], x_len, {}
+    def evaluate(self, parameters, config):
+      return 0,0,{"no evaluation":0}
+# Start Flower client
+fl.client.start_numpy_client(server_address="172.31.18.91:8080", client=flClient())
---
+++ iot_transfer/server.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python3.7
+
+import flwr as fl
+from typing import Callable, Dict, Optional, Tuple
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error, mean_absolute_percentage_error
+import sys
+import tensorflow as tf
+from tensorflow.keras import layers
+import pandas as pd
+import numpy as np
+import pickle
+
+MIN_AVAILABLE_CLIENTS=int(sys.argv[1])
+NUM_ROUND=5
+NUM_EPOCHS = 1
+
+#data load from boto3
+def divide_list(arr,n):
+    for i in range(0,len(arr),n):
+        yield arr[i:i+n]
+
+#read file from s3
+import boto3
+client = boto3.client('s3')
+paginator = client.get_paginator('list_objects_v2')
+response_iterator = paginator.paginate(Bucket='federatedlearning2')
+file_n =0
+for page in response_iterator:
+    for content in page['Contents']:
+        if content['Key'][-4:]=='.pkl' and content['Key'][0] == 't':
+            file_n+=1
+
+#read file from local
+#import os
+#path = '/home/ec2-user/data/ts_data'
+#file_list=os.listdir(path)
+#file_n=0
+#for content in file_list:
+#    if content[-4:]=='.pkl':
+#        file_n+=1
+
+# 데이터 수를 노드 수에 맞추기 위한 코드
+file_n = 54 #고정
+
+# 나머지 분배코드, 사용x
+if file_n % MIN_AVAILABLE_CLIENTS > 0:
+    DIV = file_n // MIN_AVAILABLE_CLIENTS +1
+else:
+    DIV = file_n//MIN_AVAILABLE_CLIENTS
+print(file_n,DIV)
+#test
+#fit strategy
+def get_on_fit_config_fn() -> Callable[[int], Dict[str, str]]:
+    def fit_config(rnd:int) -> Dict[str,str]:
+        config = {"epoch" : NUM_EPOCHS, "round":rnd, "file_n":file_n, "div":DIV, "n_round":NUM_ROUND,"n_clients": MIN_AVAILABLE_CLIENTS}
+        return config
+    return fit_config
+#server side evaluation
+def get_eval_fn(model):
+    
+    # The `evaluate` function will be called after every round
+    def evaluate(weights: fl.common.Weights) -> Optional[Tuple[float, float]]:
+        
+        new_weights = model.get_weights()[:3] + weights
+        
+        model.set_weights(new_weights)  # Update model with the latest parameters
+
+        # testdata
+        df_val_ts = pd.read_pickle('/home/ec2-user/fl_project/data/ts_file0.pkl')
+        features = df_val_ts.drop('y', axis=1).values
+        features_arr = np.array(features)
+
+        # reshape for input into LSTM. Batch major format.
+        num_records = len(df_val_ts.index)
+        features_batchmajor = features_arr.reshape(num_records, -1, 1)
+
+        # Scaled to work with Neural networks.
+        with open('/home/ec2-user/fl_project/data/scaler_train.pickle','rb') as f:
+            scaler = pickle.load(f)
+
+        y_pred = model.predict(features_batchmajor).reshape(-1, )
+        y_pred = scaler.inverse_transform(y_pred.reshape(-1, 1)).reshape(-1 ,)
+
+        y_act = df_val_ts['y'].values
+        y_act = scaler.inverse_transform(y_act.reshape(-1, 1)).reshape(-1 ,)
+        #mean_squared_error, r2_score, mean_absolute_error, mean_absolute_percentage_error, mean_squared_log_error
+
+        mse = mean_squared_error(y_act, y_pred)
+        r2 =r2_score(y_act, y_pred)
+        mae = mean_absolute_error(y_act, y_pred)
+        mape = mean_absolute_percentage_error(y_act, y_pred)
+        f = open('/home/ec2-user/result.txt','w')
+        f.write("mse : "+str(mse)+"\n")
+        f.write("rmse : "+str(np.sqrt(mse))+"\n")
+        f.write("r2 : "+str(r2)+"\n")
+        f.write("mae : "+str(mae)+"\n")
+        f.write("mape : "+str(mape)+"\n")
+        f.close()
+        return mse, {"r2_score":r2}
+
+    return evaluate
+
+# model
+ts_inputs = tf.keras.Input(shape=(1008,1))
+x = layers.LSTM(units=10)(ts_inputs)
+x = layers.Dropout(0.2)(x)
+outputs = layers.Dense(1, activation='linear')(x)
+model = tf.keras.Model(inputs=ts_inputs, outputs=outputs)
+
+model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.01),
+          loss=tf.keras.losses.MeanSquaredError(),
+          metrics=['mse'])
+
+with open('/home/ec2-user/fl_project/data/full_lstm.pkl','rb') as f:
+  w = pickle.load(f)
+model.set_weights(w)
+
+strategy = fl.server.strategy.FedAvg(
+    fraction_fit=1,  # Sample 10% of available clients for the next round
+    min_fit_clients=MIN_AVAILABLE_CLIENTS,  # Minimum number of clients to be sampled for the next round
+    min_available_clients=MIN_AVAILABLE_CLIENTS,  # Minimum number of clients that need to be connected to the server before a training round can start
+    min_eval_clients=MIN_AVAILABLE_CLIENTS, # default = 2
+    on_fit_config_fn=get_on_fit_config_fn(),
+    eval_fn = get_eval_fn(model)
+)
+
+#federated learning
+print('start')
+fl.server.start_server(config={"num_rounds": NUM_ROUND},strategy=strategy)
+
